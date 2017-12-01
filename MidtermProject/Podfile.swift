e8d3dd0fd621617f010bc680a682a73e43b6d0e5
Joshua T. Berry <s1055270@hh305-23ml56165.monmouth.edu>
2017-12-01 13:18:49 -0500

    adding

---
+++ MidtermProject/Podfile.swift
@@ -0,0 +1,16 @@
+{\rtf1\ansi\ansicpg1252\cocoartf1504\cocoasubrtf830
+{\fonttbl\f0\fnil\fcharset0 Consolas;}
+{\colortbl;\red255\green255\blue255;\red27\green31\blue34;\red244\green246\blue249;\red6\green33\blue79;
+\red7\green68\blue184;}
+{\*\expandedcolortbl;;\cssrgb\c14118\c16078\c18039;\cssrgb\c96471\c97255\c98039;\cssrgb\c1176\c18431\c38431;
+\cssrgb\c0\c36078\c77255;}
+\margl1440\margr1440\vieww9000\viewh8400\viewkind0
+\deftab720
+\pard\pardeftab720\partightenfactor0
+
+\f0\fs27\fsmilli13600 \cf2 \cb3 \expnd0\expndtw0\kerning0
+source \cf4 'https://github.com/CocoaPods/Specs.git'\cf2 \
+platform \cf5 :ios\cf2 , \cf4 '11.0'\cf2 \
+use_frameworks!\
+\
+pod \cf4 'Eureka'}
