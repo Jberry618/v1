f1bf2db54b02e39ec2b3cefd5653e1f002bb375b
Joshua Berry <josh.berry618@gmail.com>
2017-12-10 21:33:45 -0500

    ok

--- MidtermProject/Controllers/DriverSignInViewController.swift
+++ MidtermProject/Controllers/DriverSignInViewController.swift
@@ -15,6 +15,8 @@ class DriverSignInViewController: UIViewController, MKMapViewDelegate, CLLocatio
     @IBOutlet weak var signIn: UIButton!
     @IBOutlet weak var mapView: MKMapView!
     
+
+    
     @IBAction func signIn(_ sender: UIButton) {
         OkAlert(withTitle: "Successful", andMessage: "Thank you, freight status and location has been updated.")
     }
@@ -27,11 +29,6 @@ class DriverSignInViewController: UIViewController, MKMapViewDelegate, CLLocatio
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        setupLocationTracking()
-        //Zoom to user location
-        let noLocation = CLLocationCoordinate2D()
-        let viewRegion = MKCoordinateRegionMakeWithDistance(noLocation, 200, 200)
-        mapView.setRegion(viewRegion, animated: false)
 
         // Do any additional setup after loading the view.
     }
@@ -43,24 +40,6 @@ class DriverSignInViewController: UIViewController, MKMapViewDelegate, CLLocatio
     // Location Managing starts here
     let locationManager = CLLocationManager()
     
-    func setupLocationTracking () {
-        
-        locationManager.desiredAccuracy = kCLLocationAccuracyBest
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.delegate = self
-        locationManager.startUpdatingLocation()
-        
-    }
-    
-    func locationManager(manager: CLLocationManager!, didUpdateLocations locations: [AnyObject]!) {
-        print ("location udaated")
-        let location = locations.last as! CLLocation
-        print (location)
-        let center = CLLocationCoordinate2D(latitude: location.coordinate.latitude, longitude: location.coordinate.longitude)
-        var region = MKCoordinateRegion(center: center, span: MKCoordinateSpan(latitudeDelta: 0.1, longitudeDelta: 0.1))
-        region.center = mapView.userLocation.coordinate
-        mapView.setRegion(region, animated: true)
-    }
     
     func OkAlert (withTitle title: String, andMessage message: String ) {
         let alert = UIAlertController(title: title , message: message, preferredStyle: .actionSheet)
